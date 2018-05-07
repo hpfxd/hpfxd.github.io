0abe1fe220d86e4e06a68cb4c206302c24aca87d
nathan <31641700+hpfxd@users.noreply.github.com>
2018-05-07 18:00:10 -0400

    Create 000fix.js

---
+++ 000fix.js
@@ -0,0 +1,9 @@
+/*
+000fix.js
+
+ /----------------\
+| Twitter @hpfkys  |
+| Discord hpf#1337 |
+ \----------------/
+ */
+ $(document).ready(function(){$('body').find('img[src$="https://cdn.rawgit.com/000webhost/logo/e9bd13f7/footer-powered-by-000webhost-white2.png"]').remove();});
