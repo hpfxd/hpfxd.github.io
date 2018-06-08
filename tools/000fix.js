b523bd5160ef4e0ef7a1de0f89d032cba0a85d13
hpf <nathan@rape.lol>
2018-06-08 01:09:51 -0400

    wew !

--- tools/000fix.js
+++ tools/000fix.js
@@ -1,9 +1,9 @@
 /*
 000fix.js
+Removes 000webhost branding.
 
- /----------------\
-| Twitter @hpfkys  |
-| Discord hpf#1337 |
- \----------------/
+ /------------------------\
+| https://hpfxd.tooger.xyz |
+ \------------------------/
  */
  $(document).ready(function(){$('body').find('img[src$="https://cdn.rawgit.com/000webhost/logo/e9bd13f7/footer-powered-by-000webhost-white2.png"]').remove();});
