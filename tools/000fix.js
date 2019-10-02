aa01da4f822c3d1c2f5de22ab7ca6ae7f924b4e3
hpfxd <me@hpfxd.nl>
2019-10-02 03:50:24 -0400

    v2

--- tools/000fix.js
+++
@@ -1,9 +0,0 @@
-/*
-000fix.js
-Removes 000webhost branding.
-
- /------------------------\
-| https://hpfxd.tooger.xyz |
- \------------------------/
- */
- $(document).ready(function(){$('body').find('img[src$="https://cdn.rawgit.com/000webhost/logo/e9bd13f7/footer-powered-by-000webhost-white2.png"]').remove();});
