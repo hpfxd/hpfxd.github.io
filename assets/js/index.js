9e5cfb701226c52bc3cc0df7ab4540365464befc
hpfxd <me@hpfxd.com>
2022-10-31 05:22:25 -0400

    Replace JS with actual hrefs

--- assets/js/index.js
+++
@@ -1,9 +0,0 @@
-for (const ele of document.getElementsByClassName("sm-button")) {
-	const link = ele.attributes["data-link"];
-
-	if (link) {
-		ele.onclick = () => {
-			window.open(link.value, "_blank");
-		};
-	}
-}
